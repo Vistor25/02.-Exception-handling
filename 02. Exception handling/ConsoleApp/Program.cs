67ad232d7811cedcb9d669acd93fedf5fa7db6d9
Vistor25 <victorvoronko@gmail.com>
2017-10-20 18:42:37 +0300

    developing

---
+++ 02. Exception handling/ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tas2;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string a = Console.ReadLine();
+                Console.WriteLine(ParseNumber.Parse(a));
+            }
+        }
+    }
+}
---
+++ 02. Exception handling/Tas2/ParseNumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tas2
+{
+    public static class ParseNumber
+    {
+        public static int Parse(string number)
+        {
+            int result = 0;
+            int counter = 1;
+            for (int i = number.Length-1; i >=0 ; i--)
+            {
+                result = result + ToDigit(number[i])*counter;
+                counter *= 10;
+            }
+            return result;
+        }
+
+        private static int ToDigit(char a)
+        {
+            int b = Convert.ToByte(a);
+            return b - 48;
+        }
+    }
+}
